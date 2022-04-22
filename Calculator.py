a35e08d622a5630778eb7439cb58c25d651a38ed
Tian du Toit <88508173+TDT56@users.noreply.github.com>
2022-04-22 14:21:23 +0200

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,17 +1,3 @@
-'''
-This program finds the best resistor pair in parallel to obtain a certain value.
-0.5 O being the minimum and 410 kO being the maximum 
-
-add a small letter k for kilo ohm as in 256k (without any spaces)
-111 and 111k could results in a different resistor pair because it is based on the actual resistors in the lab
-
-the program also allows for overshoot because it is not possible to obtain the exact value
-thus is you input 256 you may get a answer (like 256.75) slighty higher
-if you wish to never get overshoot just set the overshoot variable below to zero
-
-ENJOY!
-'''
-
 #how many ohms are the program allowed to overshoot your given value
 overshoot = 0.15
 
